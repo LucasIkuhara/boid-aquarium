199fefc2ce4b9bde831a7133e565bf18c37ab40b
Lucas Ikuhara <ikuhara@ibm.com>
2022-10-16 21:13:24 -0300

    fix: fragment shader syntax

--- src/shaders/boid_frag.js
+++ src/shaders/boid_frag.js
@@ -7,26 +7,18 @@ uniform vec2 end;
 uniform vec2 resolution;
 
 void main () {
-    precision mediump float;
-    uniform vec4 color;
-    uniform vec3 start;
-    uniform vec2 end;
-    uniform vec2 resolution;
-    
-    void main () {
 
-        // Compute position
-        vec2 position = resolution.yx / 2.0 + start.xy;
+    // Compute position
+    vec2 position = resolution.yx / 2.0 + start.xy;
 
-        // 5%
-        float distThresholdInPixels = ((resolution[0] + resolution[1]) / 2.0) * 0.006;
+    // 5%
+    float distThresholdInPixels = ((resolution[0] + resolution[1]) / 2.0) * 0.006;
 
-        if (distance(position, gl_FragCoord.xy) > distThresholdInPixels) {
-        discard;
-        }
-
-        gl_FragColor = vec4(position, 0.0, 1.0);
-    
+    if (distance(position, gl_FragCoord.xy) > distThresholdInPixels) {
+    discard;
     }
+
+    gl_FragColor = vec4(position, 0.0, 1.0);
+
 }
 `
